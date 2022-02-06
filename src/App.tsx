74f34c20b9f186d152067725c3bb840f37d45375
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-02-06 17:31:51 -0300

    feat(improve-ui): improve ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
+import { Container } from "@material-ui/core";
 import React, { ReactElement } from "react";
-import { graphql, useLazyLoadQuery } from "react-relay";
-
-import { AppQuery } from "./__generated__/AppQuery.graphql";
+import { Flex } from "rebass";
+import Header from "./components/Header";
+import ShipsCard from "./components/ShipsCard";
+import Title from "./components/Title";
 
 export default function App(): ReactElement {
-  const data = useLazyLoadQuery<AppQuery>(
-    graphql`
-      query AppQuery {
-        ships {
-          id
-          name
-          image
-        }
-      }
-    `,
-    {}
-  );
-
   return (
-    <>
-      <h1>Vite + Relay</h1>
-      <h2>
-        <a href="https://www.spacex.com" target="_blank">
-          SpaceX
-        </a>{" "}
-        Data Viewer
-      </h2>
-      <h2 id="ships-heading">Ships</h2>
-      <ul aria-labelledby="ships-heading">
-        {data.ships?.map((ship) => (
-          <>
-            <li key={ship?.id}>{ship?.name}</li>
-            <img src={ship?.image} alt={ship?.name} width="100" height="100" />
-          </>
-        ))}
-      </ul>
-    </>
+    <Flex justifyContent={'center'} alignItems={'center'} flexDirection={'column'}>
+      <Container>
+      <Header />
+      <Title />
+      <ShipsCard />
+      </Container>
+    </Flex>
   );
 }
