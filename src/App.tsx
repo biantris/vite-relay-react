2184db53492d4c8a70afc528b3481cefbd4b851c
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-02-06 12:28:17 -0300

    feat(relay): add relay

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { ReactElement } from "react";
+import { graphql, useLazyLoadQuery } from "react-relay";
 
-function App() {
+import { AppQuery } from "./__generated__/AppQuery.graphql";
+
+export default function App(): ReactElement {
+  const data = useLazyLoadQuery<AppQuery>(
+    graphql`
+      query AppQuery {
+        ships {
+          id
+          name
+          image
+        }
+      }
+    `,
+    {}
+  );
+
   return (
-    <h1>Hello Vite + React!</h1>
-  )
+    <>
+      <h1>Vite + Relay</h1>
+      <h2>
+        <a href="https://www.spacex.com" target="_blank">
+          SpaceX
+        </a>{" "}
+        Data Viewer
+      </h2>
+      <h2 id="ships-heading">Ships</h2>
+      <ul aria-labelledby="ships-heading">
+        {data.ships?.map((ship) => (
+          <>
+            <li key={ship?.id}>{ship?.name}</li>
+            <img src={ship?.image} alt={ship?.name} width="100" height="100" />
+          </>
+        ))}
+      </ul>
+    </>
+  );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { RelayEnvironmentProvider } from "react-relay";
+import { RelayEnvironment } from "./relay";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <Suspense fallback={<h1>Loading Data...</h1>}>
+        <App />
+      </Suspense>
+    </RelayEnvironmentProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root"),
+);
